fb95d5cd0d5d22d1b78fbd488dd6861517aff077
Portatil <>
2018-11-10 21:25:17 -0500

    Pre alfa: Commit para merge

---
+++ Assets/Scripts/Mecanicas/BotonDePared.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	/// <summary>
+	/// Este código se pone en el botón de pared, cuando el jugador esté cerca, 
+	/// que se imprima en consola se puede usar, si se puede usar el botón y oprime E entonces 
+	/// imprime el botón ha sido presionado.
+	/// </summary>
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
