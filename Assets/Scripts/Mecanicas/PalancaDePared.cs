4517e2b19f468d19aa6cae5d907851c28051be93
Casa <>
2018-11-22 19:46:27 -0500

    Se incopora el modelo del nivel 3, y un nuevo modelo de trampa y para una palanca de pared, se crea el script de la palanca de pared

---
+++ Assets/Scripts/Mecanicas/PalancaDePared.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+using UnityEditor;
+
+public class PalancaDePared : MonoBehaviour {
+
+	/// <summary>
+	/// 
+	/// </summary>
+	bool inTrigger;
+
+	public GameObject FBX;
+
+	public GameObject Trigger;
+
+	public GameObject nivel;
+
+	public AnimationClip AnimacionAsignada;
+
+    public bool SePuedeRepetir = false;
+
+    string nombre;
+
+    int layer;
+
+	Animation AnimacionBoton;
+
+    public AnimationClip[] ArrayAnimaciones;
+
+    bool activada = false;
+
+
+    void Start () 
+	{
+        AnimacionBoton = FBX.GetComponent<Animation>();
+
+        for (int i = 0; i < ArrayAnimaciones.Length; i++)
+        {
+            if (AnimacionAsignada == ArrayAnimaciones[i])
+            {
+                nombre = AnimacionAsignada.name;
+                layer = i;
+            }
+
+
+        }
+
+
+    }   
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		if(Input.GetKeyDown(KeyCode.E) && Trigger.GetComponent<RevisionTrigger>().EstaEnTrigger && activada==false)
+		{
+            if (!AnimacionBoton.IsPlaying("Accionar Boton"))
+            {
+                int control = 0;
+
+                Debug.Log("Botón presionado");
+                AnimacionBoton.Play("Accionar Boton");
+                activada = true;
+
+                if (AnimacionAsignada != null)
+                {
+                    if (!SePuedeRepetir && control==0)
+                    {
+
+                      
+                            control = 1;
+                            nivel.GetComponent<Animator>().Play(nombre, layer);
+                       
+                    }
+
+                }
+            }
+
+            else
+            {
+
+                Debug.Log("Botón en uso");
+
+            }
+			
+
+		}
+
+		
+	}
+
+}
