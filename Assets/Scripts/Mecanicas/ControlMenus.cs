758d6fd3ae230c1e2991719e12987eb7661baaa0
Portatil <>
2018-11-22 19:45:32 -0500

    Se dejan listas las funcionalidades básicas del menú

--- Assets/Scripts/Mecanicas/ControlMenus.cs
+++ Assets/Scripts/Mecanicas/ControlMenus.cs
@@ -2,19 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using ARFC;
+using UnityEngine.UI;
 
 public class ControlMenus : MonoBehaviour
 {
 
+	public GameObject PausaMenu;
+
+	public Button Continuar;
+
+	
 	void Start ()
 	{
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-				
+
+		Pausa();
+
 	}
 
 	public void CargarScena(int i)
@@ -32,6 +41,27 @@ public class ControlMenus : MonoBehaviour
 	public void DesactivarCanvas(GameObject canvasDesactivado)
 	{
 		canvasDesactivado.SetActive(false);
+	}
+
+	public void Pausa()
+    {	
+		if (Time.timeScale == 1.0f && Input.GetKeyDown(KeyCode.P))
+        {
+			PausaMenu.SetActive(true);
+            Time.timeScale = 0.0f;
+			GameObject.FindWithTag("Player").GetComponent<FPController>().enabled = false;
+			
+            
+        }
+    
+	}
+
+	public void QuitarPausar()
+	{
+
+		Time.timeScale = 1.0f;
+		GameObject.FindWithTag("Player").GetComponent<FPController>().enabled = true;
+
 	}
 
 	public void QuitarJuego()
